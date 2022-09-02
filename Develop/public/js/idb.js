1031a67d987c47ca738ec5bbc2579830a7b57a58
Christa <christanewing47@gmail.com>
2022-09-02 06:57:38 -0400

    better personal readability

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -1,4 +1,5 @@
 /* S T A N D A R D I Z I N G  B R O W S E R S */
+let db;
 
 const indexedDB =
   window.indexedDB ||
@@ -6,18 +7,6 @@ const indexedDB =
   window.webkitIndexedDB ||
   window.msIndexedDB ||
   window.shimIndexedDB;
-
-let db;
-
-const database = "budget";
-const objectStore = "transactions";
-
-const request = indexedDB.open(database, 1);
-
-request.onupgradeneeded = ({ target }) => {
-  let db = target.result;
-  db.createObjectStore(objectStore, { autoIncrement: true });
-};
 
 // M A K I N G  S U R E  T H E  A P P  I S  O N L I N E
 request.onsuccess = ({ target }) => {
@@ -31,10 +20,23 @@ request.onerror = function (event) {
   console.log("Mistakes were made! " + event.target.errorCode);
 };
 
+const database = "budget";
+const objectStore = "transactions";
+
+const request = indexedDB.open(database, 1);
+
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore(objectStore, { autoIncrement: true });
+};
+
 // S A V E  R E C O R D
 function saveRecord(record) {
+  // C R E A T E  T R A N S A C T I O N
   const transaction = db.transaction([objectStore], "readwrite");
+  // A C C E S S  S T O R E
   const store = transaction.objectStore(objectStore);
+  // A D D  N E W  R E C O R D
   store.add(record);
 }
 
