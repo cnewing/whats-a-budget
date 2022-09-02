e9036e7f9403811f9aa7d0fdc7e4a47229ed0f96
Christa <christanewing47@gmail.com>
2022-09-02 06:44:59 -0400

    u

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -5,22 +5,6 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 const DATA_CACHE_NAME = "data-cache-" + VERSION;
 
 // F I L E S  F O R  C A C H I N G
-const FILES_TO_CACHE = [
-  "/",
-  "./index.html",
-  "./css/styles.css",
-  "./js/idb.js",
-  "./js/index.js",
-  "./manifest.json",
-  "./icons/icon-72x72.png",
-  "./icons/icon-96x96.png",
-  "./icons/icon-128x128.png",
-  "./icons/icon-144x144.png",
-  "./icons/icon-152x152.png",
-  "./icons/icon-192x192.png",
-  "./icons/icon-384x384.png",
-  "./icons/icon-512x512.png",
-];
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
